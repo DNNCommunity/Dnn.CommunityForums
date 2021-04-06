2dd91fe7e8b3364dd34e11c008c8d19fe841ae28
Jon Bockhorst <jbockhorst@engagesoftware.com>
2021-04-06 12:01:50 -0500

    Use getAttribute('class') instead of className to support SVG elements

--- scripts/afadmin.js
+++ scripts/afadmin.js
@@ -367,14 +367,15 @@ amcp.Utils = {
         var retVal = new Array();
         var elements = document.getElementsByTagName("*");
         for (var i = 0; i < elements.length; i++) {
-            if (elements[i].className.indexOf(" ") >= 0) {
-                var classes = elements[i].className.split(" ");
+            var className = elements[i].getAttribute('class');
+            if (className && className.indexOf(" ") >= 0) {
+                var classes = className.split(" ");
                 for (var j = 0; j < classes.length; j++) {
                     if (classes[j] == clsName) {
                         retVal.push(elements[i]);
                     };
                 };
-            } else if (elements[i].className == clsName) {
+            } else if (className == clsName) {
                 retVal.push(elements[i]);
             };
         };
