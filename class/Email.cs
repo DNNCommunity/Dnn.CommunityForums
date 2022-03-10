b2c833f31d7e1de0c207fe939666527883e54136
John Henley <john.henley@danalytics.com>
2022-03-10 16:05:12 -0500

    portal-specific SMTP configuration when sending email

--- class/Email.cs
+++ class/Email.cs
@@ -46,8 +46,7 @@ namespace DotNetNuke.Modules.ActiveForums
         public bool UseQueue = false;
 
 		public static void SendEmail(int templateId, int portalId, int moduleId, int tabId, int forumId, int topicId, int replyId, string comments, Author author)
-		{
-
+		{	
 			var portalSettings = (Entities.Portals.PortalSettings)(HttpContext.Current.Items["PortalSettings"]);
 			var mainSettings = DataCache.MainSettings(moduleId);
 		    var sTemplate = string.Empty;
