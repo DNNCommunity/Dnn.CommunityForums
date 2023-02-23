59877160800327a2c7c8b1e7b28eb8adea1a77fc
John Henley <john.henley@danalytics.com>
2023-02-23 14:53:47 -0500

    use User's last reply date in flood control

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -52,9 +52,9 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("07.00.04")]
+[assembly: AssemblyVersion("07.00.11")]
 
-[assembly: AssemblyFileVersion("07.00.04")]
+[assembly: AssemblyFileVersion("07.00.11")]
 
 [assembly: WebResource("DotNetNuke.Modules.ActiveForums.CustomControls.Resources.cb.js", "text/javascript")]
 [assembly: WebResource("DotNetNuke.Modules.ActiveForums.scripts.afadmin.properties.js", "text/javascript")]
