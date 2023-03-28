d4d84be0378619f53d0dedad1e47d199ae942b60
John Henley <john.henley@danalytics.com>
2023-03-28 17:19:36 -0400

    web api implementation / controllerbase

---
+++ Services/Controllers/ControllerBase.cs
@@ -0,0 +1,49 @@
+﻿//
+// Community Forums
+// Copyright (c) 2013-2021
+// by DNN Community
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+// documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+// the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and 
+// to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions 
+// of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+// CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+// DEALINGS IN THE SOFTWARE.
+// 
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DotNetNuke.Web.Api;
+using DotNetNuke.Entities.Portals;
+using DotNetNuke.Instrumentation;
+
+namespace DotNetNuke.Modules.ActiveForums.Services
+{
+/// <summary>
+/// <inheritdoc/>
+/// </summary>
+/// <typeparam name="T"></typeparam>
+    [SupportedModules(Globals.ModuleName)] /* this MUST match DesktopModule.ModuleName so use new constant */
+    public class ControllerBase<T> : DnnApiController
+    {
+        private static readonly ILog Logger = LoggerSource.Instance.GetLogger(typeof(T));
+        /// <summary>
+        /// provide a simple method for testing
+        /// </summary>
+        /// <returns></returns>
+        [AllowAnonymous]
+        [HttpGet]
+        public HttpResponseMessage HelloWorld()
+        {
+            Logger.Info("Hello World!");
+            return Request.CreateResponse(HttpStatusCode.OK, "Hello World!");
+        }
+    }
+}
