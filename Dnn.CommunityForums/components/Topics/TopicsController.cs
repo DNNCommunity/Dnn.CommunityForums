6dca462c77e864c556b8b1d1b3edd041d41da7db
John Henley <john.henley@danalytics.com>
2023-07-28 18:10:19 -0400

    additional rebranding updates remove extensionpackage rebranding package name change logic update assembly to 08.00.00 update build targets to exclude GhostDoc.xml update build targets to package ActiveForumsViewer as ForumsViewer add cleanup file for 08.00.00 to remove ActiveForumsViewer manifest (now ForumsViewer) update .gitignore for /BuildScripts/ICSharpCode.SharpZipLib.dll and /BuildScripts/MSBuild.Community.Tasks.dll update manifest for new logo add exclude to build targets to exclude illustrator logo source files

--- Dnn.CommunityForums/components/Topics/TopicsController.cs
+++ Dnn.CommunityForums/components/Topics/TopicsController.cs
@@ -591,8 +591,6 @@ namespace DotNetNuke.Modules.ActiveForums
                         var fc = new ForumsConfig();
                         fc.Install_Or_Upgrade_MoveTemplates();
                         fc.Install_Or_Upgrade_RenameThemeCssFiles();
-
-                        fc.UpdateExtensionBranding();
                     }
                     catch (Exception ex)
                     {
