30614a688a2b4d3c24bb50cfab39b03cc352fd30
John Henley <john.henley@danalytics.com>
2023-12-12 17:55:30 -0500

    RepositoryControllerBase for Subscriptions

---
+++ Dnn.CommunityForums/Entities/ContentInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetNuke.Modules.ActiveForums.Entities
+{
+    internal class ContentInfo
+    {
+    }
+}
