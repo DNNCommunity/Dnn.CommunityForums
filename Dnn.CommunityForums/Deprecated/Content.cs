5abb98f55197f4964748720c6b0f5573e492ae7e
John Henley <john.henley@danalytics.com>
2024-02-13 21:33:11 +0000

    deprecated code

---
+++ Dnn.CommunityForums/Deprecated/Content.cs
@@ -0,0 +1,11 @@
+﻿using DotNetNuke.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DotNetNuke.Modules.ActiveForums.DAL2
+{
+   [Obsolete("Deprecated in Community Forums. Removed in 10.00.00. Use DotNetNuke.Modules.ActiveForums.Entities.ContentInfo.")]
+    class Content : DotNetNuke.Modules.ActiveForums.Entities.ContentInfo { }
+}
