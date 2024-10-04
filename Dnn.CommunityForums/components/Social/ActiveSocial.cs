9bdeb5fef5180b3febca6bf640a923a391e222ca
John Henley <john.henley@danalytics.com>
2024-10-04 21:31:38 +0000

    ENH: Update journal item when updating a post

--- Dnn.CommunityForums/components/Social/ActiveSocial.cs
+++ Dnn.CommunityForums/components/Social/ActiveSocial.cs
@@ -39,75 +39,7 @@ namespace DotNetNuke.Modules.ActiveForums
 
         internal void AddTopicToJournal(int portalId, int moduleId, int tabId, int forumId, int topicId, int userId, string uRL, string subject, string summary, string body, string readRoles, int socialGroupId)
         {
-            var ji = new JournalItem
-            {
-                PortalId = portalId,
-                ProfileId = userId,
-                UserId = userId,
-                Title = subject,
-                ItemData = new ItemData { Url = uRL },
-            };
-            if (string.IsNullOrEmpty(summary))
-            {
-                summary = Utilities.StripHTMLTag(body);
-                if (summary.Length > 150)
-                {
-                    summary = summary.Substring(0, 150) + "...";
-                }
-            }
-
-            ji.Summary = summary;
-            ji.Body = Utilities.StripHTMLTag(body);
-            ji.JournalTypeId = 5;
-            ji.ObjectKey = string.Format("{0}:{1}", forumId.ToString(), topicId.ToString());
-            if (JournalController.Instance.GetJournalItemByKey(portalId, ji.ObjectKey) != null)
-            {
-                JournalController.Instance.DeleteJournalItemByKey(portalId, ji.ObjectKey);
-            }
-
-            string roles = string.Empty;
-            if (!string.IsNullOrEmpty(readRoles))
-            {
-                if (readRoles.Contains("|"))
-                {
-                    roles = readRoles.Substring(0, readRoles.IndexOf("|", StringComparison.Ordinal) - 1);
-                }
-            }
-
-            foreach (string s in roles.Split(';'))
-            {
-                if ((s == DotNetNuke.Common.Globals.glbRoleAllUsers) | (s == DotNetNuke.Common.Globals.glbRoleUnauthUser))
-                {
-                    /* cjh - securityset was null and throwing an error, thus journal items weren't added */
-                    if ((ji.SecuritySet != null) && !ji.SecuritySet.Contains("E,"))
-                    {
-                        ji.SecuritySet += "E,";
-                    }
-                }
-                else
-                {
-                    ji.SecuritySet += "R" + s + ",";
-                }
-            }
-
-            if (socialGroupId > 0)
-            {
-                ji.SocialGroupId = socialGroupId;
-            }
-
-            JournalController.Instance.SaveJournalItem(journalItem: ji, module: DotNetNuke.Entities.Modules.ModuleController.Instance.GetModule(moduleId, tabId, true));
-        }
-
-        [Obsolete("Deprecated in Community Forums 9.0.0. No interface with Active Social.")]
-        public void AddReplyToJournal(int portalId, int moduleId, int forumId, int topicId, int replyId, int userId, string uRL, string subject, string summary, string body, int securityOption, string readRoles, int socialGroupId)
-        {
-            this.AddReplyToJournal(portalId: portalId, moduleId: moduleId, tabId: -1, forumId: forumId, topicId: topicId, replyId: replyId, userId: userId, uRL: uRL, subject: subject, summary: summary, body: body, readRoles: readRoles, socialGroupId: socialGroupId);
-        }
-
-        internal void AddReplyToJournal(int portalId, int moduleId, int tabId, int forumId, int topicId, int replyId, int userId, string uRL, string subject, string summary, string body, string readRoles, int socialGroupId)
-        {
-            // make sure that this is a User before trying to create a journal item, you can't post a JI without
-            if (userId > 0)
+            try
             {
                 var ji = new JournalItem
                 {
@@ -128,8 +60,8 @@ namespace DotNetNuke.Modules.ActiveForums
 
                 ji.Summary = summary;
                 ji.Body = Utilities.StripHTMLTag(body);
-                ji.JournalTypeId = 6;
-                ji.ObjectKey = string.Format("{0}:{1}:{2}", forumId.ToString(), topicId.ToString(), replyId.ToString());
+                ji.JournalTypeId = 5;
+                ji.ObjectKey = $"{forumId}:{topicId}";
                 if (JournalController.Instance.GetJournalItemByKey(portalId, ji.ObjectKey) != null)
                 {
                     JournalController.Instance.DeleteJournalItemByKey(portalId, ji.ObjectKey);
@@ -149,7 +81,7 @@ namespace DotNetNuke.Modules.ActiveForums
                     if ((s == DotNetNuke.Common.Globals.glbRoleAllUsers) | (s == DotNetNuke.Common.Globals.glbRoleUnauthUser))
                     {
                         /* cjh - securityset was null and throwing an error, thus journal items weren't added */
-                        if ((ji.SecuritySet != null) && (!ji.SecuritySet.Contains("E,")))
+                        if ((ji.SecuritySet != null) && !ji.SecuritySet.Contains("E,"))
                         {
                             ji.SecuritySet += "E,";
                         }
@@ -165,7 +97,137 @@ namespace DotNetNuke.Modules.ActiveForums
                     ji.SocialGroupId = socialGroupId;
                 }
 
-                JournalController.Instance.SaveJournalItem(journalItem: ji, module: DotNetNuke.Entities.Modules.ModuleController.Instance.GetModule(moduleId: moduleId, tabId: tabId, ignoreCache: false));
+                JournalController.Instance.SaveJournalItem(journalItem: ji, module: DotNetNuke.Entities.Modules.ModuleController.Instance.GetModule(moduleId, tabId, true));
+            }
+            catch (Exception ex)
+            {
+                DotNetNuke.Services.Exceptions.Exceptions.LogException(ex);
+            }
+        }
+
+        internal void UpdateJournalItemForPost(int portalId, int moduleId, int tabId, int forumId, int topicId, int replyId, int userId, string uRL, string subject, string summary, string body)
+        {
+            try
+            {
+                string objectKey = replyId <= 0 ? $"{forumId}:{topicId}" : $"{forumId}:{topicId}:{replyId}";
+                var ji = JournalController.Instance.GetJournalItemByKey(portalId, objectKey);
+                if (ji != null)
+                {
+                    if (string.IsNullOrEmpty(summary))
+                    {
+                        summary = Utilities.StripHTMLTag(body);
+                        if (summary.Length > 150)
+                        {
+                            summary = summary.Substring(0, 150) + "...";
+                        }
+                    }
+
+                    ji.Title = subject;
+                    ji.Summary = summary;
+                    ji.ItemData = new ItemData { Url = uRL };
+                    ji.Body = Utilities.StripHTMLTag(body);
+                    ji.DateUpdated = DateTime.UtcNow;
+                    JournalController.Instance.UpdateJournalItem(journalItem: ji, module: DotNetNuke.Entities.Modules.ModuleController.Instance.GetModule(moduleId, tabId, true));
+                }
+            }
+            catch (Exception ex)
+            {
+                DotNetNuke.Services.Exceptions.Exceptions.LogException(ex);
+            }
+        }
+
+        internal void DeleteJournalItemForPost(int portalId, int forumId, int topicId, int replyId)
+        {
+            try
+            {
+                var objectKey = replyId <= 0 ? $"{forumId}:{topicId}" : $"{forumId}:{topicId}:{replyId}";
+                var ji = JournalController.Instance.GetJournalItemByKey(portalId, objectKey);
+                if (ji != null)
+                {
+                    JournalController.Instance.DeleteJournalItemByKey(portalId, objectKey);
+                }
+            }
+            catch (Exception ex)
+            {
+                DotNetNuke.Services.Exceptions.Exceptions.LogException(ex);
+            }
+        }
+
+        [Obsolete("Deprecated in Community Forums 9.0.0. No interface with Active Social.")]
+        public void AddReplyToJournal(int portalId, int moduleId, int forumId, int topicId, int replyId, int userId, string uRL, string subject, string summary, string body, int securityOption, string readRoles, int socialGroupId)
+        {
+            this.AddReplyToJournal(portalId: portalId, moduleId: moduleId, tabId: -1, forumId: forumId, topicId: topicId, replyId: replyId, userId: userId, uRL: uRL, subject: subject, summary: summary, body: body, readRoles: readRoles, socialGroupId: socialGroupId);
+        }
+
+        internal void AddReplyToJournal(int portalId, int moduleId, int tabId, int forumId, int topicId, int replyId, int userId, string uRL, string subject, string summary, string body, string readRoles, int socialGroupId)
+        {
+            try
+            {
+                // make sure that this is a User before trying to create a journal item, you can't post a JI without
+                if (userId > 0)
+                {
+                    var ji = new JournalItem
+                    {
+                        PortalId = portalId,
+                        ProfileId = userId,
+                        UserId = userId,
+                        Title = subject,
+                        ItemData = new ItemData { Url = uRL },
+                    };
+                    if (string.IsNullOrEmpty(summary))
+                    {
+                        summary = Utilities.StripHTMLTag(body);
+                        if (summary.Length > 150)
+                        {
+                            summary = summary.Substring(0, 150) + "...";
+                        }
+                    }
+
+                    ji.Summary = summary;
+                    ji.Body = Utilities.StripHTMLTag(body);
+                    ji.JournalTypeId = 6;
+                    ji.ObjectKey = $"{forumId}:{topicId}:{replyId}";
+                    if (JournalController.Instance.GetJournalItemByKey(portalId, ji.ObjectKey) != null)
+                    {
+                        JournalController.Instance.DeleteJournalItemByKey(portalId, ji.ObjectKey);
+                    }
+
+                    string roles = string.Empty;
+                    if (!string.IsNullOrEmpty(readRoles))
+                    {
+                        if (readRoles.Contains("|"))
+                        {
+                            roles = readRoles.Substring(0, readRoles.IndexOf("|", StringComparison.Ordinal) - 1);
+                        }
+                    }
+
+                    foreach (string s in roles.Split(';'))
+                    {
+                        if ((s == DotNetNuke.Common.Globals.glbRoleAllUsers) | (s == DotNetNuke.Common.Globals.glbRoleUnauthUser))
+                        {
+                            /* cjh - securityset was null and throwing an error, thus journal items weren't added */
+                            if ((ji.SecuritySet != null) && (!ji.SecuritySet.Contains("E,")))
+                            {
+                                ji.SecuritySet += "E,";
+                            }
+                        }
+                        else
+                        {
+                            ji.SecuritySet += "R" + s + ",";
+                        }
+                    }
+
+                    if (socialGroupId > 0)
+                    {
+                        ji.SocialGroupId = socialGroupId;
+                    }
+
+                    JournalController.Instance.SaveJournalItem(journalItem: ji, module: DotNetNuke.Entities.Modules.ModuleController.Instance.GetModule(moduleId: moduleId, tabId: tabId, ignoreCache: false));
+                }
+            }
+            catch (Exception ex)
+            {
+                DotNetNuke.Services.Exceptions.Exceptions.LogException(ex);
             }
         }
     }
