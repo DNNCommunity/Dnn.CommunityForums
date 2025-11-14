1985187afef0ecf045afb662fd8ea59ad1869120
John Henley <john.henley@danalytics.com>
2025-11-14 20:40:52 +0000

    remove bundled ckeditor

--- Dnn.CommunityForums/Resources/ckeditor-4.22.1/config.js
+++
@@ -1,6 +0,0 @@
-/**
- * @license Copyright (c) 2003-2025, CKSource Holding sp. z o.o. All rights reserved.
- * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
- */
-CKEDITOR.editorConfig = function( config ) {config.removeButtons = 'Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,PasteFromWord,Print,Preview,ExportPdf,NewPage,Save,Replace,Find,BGColor,TextColor,Smiley,HorizontalRule,Anchor,Unlink,BidiLtr,BidiRtl,Language,CreateDiv,CopyFormatting,RemoveFormat,Subscript,Superscript,Strike,Format';
-};
