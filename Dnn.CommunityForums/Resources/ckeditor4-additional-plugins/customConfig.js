a5bbfa9127f1f3cc47dacf6ff57cb723338ca49b
John Henley <john.henley@danalytics.com>
2025-11-21 16:27:14 +0000

    rename ckeditor plugins folder

---
+++ Dnn.CommunityForums/Resources/ckeditor4-additional-plugins/customConfig.js
@@ -0,0 +1,7 @@
+﻿CKEDITOR.plugins.addExternal('mentions', '/DesktopModules/ActiveForums/Resources/ckeditor4-additional-plugins/mentions/');
+CKEDITOR.plugins.addExternal('ajax', '/DesktopModules/ActiveForums/Resources/ckeditor4-additional-plugins/ajax/');
+CKEDITOR.plugins.addExternal('autocomplete', '/DesktopModules/ActiveForums/Resources/ckeditor4-additional-plugins/autocomplete/');
+CKEDITOR.plugins.addExternal('textmatch', '/DesktopModules/ActiveForums/Resources/ckeditor4-additional-plugins/textmatch/');
+CKEDITOR.plugins.addExternal('textwatcher', '/DesktopModules/ActiveForums/Resources/ckeditor4-additional-plugins/textwatcher/');
+CKEDITOR.plugins.addExternal('xml', '/DesktopModules/ActiveForums/Resources/ckeditor4-additional-plugins/xml/');
+CKEDITOR.plugins.addExternal('codeTag', '/DesktopModules/ActiveForums/Resources/ckeditor4-additional-plugins/codeTag/');
