3584492644a1404d8925e50016da622d08c6adee
John Henley <john.henley@danalytics.com>
2026-02-09 21:04:33 +0000

    TASK: DefaultPortalAlias test

--- Dnn.CommunityForumsTests/TestBase.cs
+++ Dnn.CommunityForumsTests/TestBase.cs
@@ -54,9 +54,11 @@ namespace DotNetNuke.Modules.ActiveForumsTests
         private Mock<IRoleController> roleController;
         private Mock<IHostController> mockHostController;
 
-        internal Mock<DotNetNuke.Entities.Modules.ModuleInfo> mockModule;
+        internal Mock<DotNetNuke.Entities.Modules.ModuleInfo> MockModule;
 
         internal Mock<DotNetNuke.Modules.ActiveForums.ModuleSettings> MainSettings;
+
+        internal string DefaultPortalAlias = "localhost/en-us";
 
         [SetUp]
 
@@ -68,6 +70,10 @@ namespace DotNetNuke.Modules.ActiveForumsTests
             serviceCollection.AddTransient<IApplicationStatusInfo>(container => Mock.Of<IApplicationStatusInfo>());
             serviceCollection.AddTransient<INavigationManager>(container => Mock.Of<INavigationManager>());
             serviceCollection.AddTransient<IHostSettingsService, HostController>();
+            
+            serviceCollection.AddSingleton<IPortalAliasService, PortalAliasController>();
+            serviceCollection.AddSingleton<IPortalAliasService, IPortalAliasService>();
+            serviceCollection.AddSingleton<IPortalAliasController, PortalAliasController>();
 
             ComponentFactory.Container = new SimpleContainer();
             this.mockRoleProvider = MockComponentProvider.CreateRoleProvider();
@@ -359,7 +365,7 @@ namespace DotNetNuke.Modules.ActiveForumsTests
 
         private void SetupModuleInfo()
         {
-            this.mockModule = new Mock<DotNetNuke.Entities.Modules.ModuleInfo>
+            this.MockModule = new Mock<DotNetNuke.Entities.Modules.ModuleInfo>
             {
                 Object =
                 {
@@ -371,7 +377,7 @@ namespace DotNetNuke.Modules.ActiveForumsTests
 
             };
 
-            this.moduleController.Setup(mc => mc.GetModule(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<bool>())).Returns(this.mockModule.Object);
+            this.moduleController.Setup(mc => mc.GetModule(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<bool>())).Returns(this.MockModule.Object);
         }
 
         private void SetupMainSettings()
